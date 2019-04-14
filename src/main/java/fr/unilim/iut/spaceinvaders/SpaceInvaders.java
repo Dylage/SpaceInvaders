ce4a65de1f8a20bcb2d8c5efff43d599ba80aecd
Quentin DELAGE <quentin.delage1@etu.unilim.fr>
2019-04-14 15:25:50 +0200

    Quelques refactoring dans la classe SpaceInvaders de la méthode toString()

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -2,32 +2,41 @@ package fr.unilim.iut.spaceinvaders;
 
 public class SpaceInvaders {
 	int longueur;
-    int hauteur;
-    Vaisseau vaisseau;
+	int hauteur;
+	Vaisseau vaisseau;
 
-    public SpaceInvaders(int longueur, int hauteur) {
-	   this.longueur = longueur;
-	   this.hauteur = hauteur;
-   }
-    
-    @Override
+	public SpaceInvaders(int longueur, int hauteur) {
+		this.longueur = longueur;
+		this.hauteur = hauteur;
+	}
+
+	public void positionnerUnNouveauVaisseau(int x, int y) {
+		this.vaisseau = new Vaisseau(x, y);
+
+	}
+
+	private boolean aUnVaisseau() {
+		return null != vaisseau;
+	}
+	
+	private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+		return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
+	}
+	
+	@Override
 	public String toString() {
 		StringBuilder espaceDeJeu = new StringBuilder();
 		for (int y = 0; y < hauteur; y++) {
 			for (int x = 0; x < longueur; x++) {
-				if (null != vaisseau && vaisseau.occupeLaPosition(x, y)) {
-				     espaceDeJeu.append('V');
+				if (aUnVaisseauQuiOccupeLaPosition(x, y)) {
+					espaceDeJeu.append('V');
 				} else {
 					espaceDeJeu.append('.');
 				}
 			}
-			espaceDeJeu.append('\n');	
+			espaceDeJeu.append('\n');
 		}
 		return espaceDeJeu.toString();
 	}
-    
-    public void positionnerUnNouveauVaisseau(int x, int y) {
-        this.vaisseau = new Vaisseau(x, y);
 
-	}
 }
