ec77e1d44b6bbfcf6ddee5ae766622585c60032c
Quentin DELAGE <quentin.delage1@etu.unilim.fr>
2019-04-14 15:47:36 +0200

    Ajout du diagramme de classe et fin de la première itération de rafactoring

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -1,6 +1,9 @@
 package fr.unilim.iut.spaceinvaders;
 
 public class SpaceInvaders {
+	private static final char MARQUE_FIN_LIGNE = '\n';
+	private static final char MARQUE_VIDE = '.';
+	private static final char MARQUE_VAISSEAU = 'V';
 	int longueur;
 	int hauteur;
 	Vaisseau vaisseau;
@@ -18,25 +21,35 @@ public class SpaceInvaders {
 	private boolean aUnVaisseau() {
 		return null != vaisseau;
 	}
-	
+
 	private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
 		return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
 	}
-	
-	@Override
-	public String toString() {
+
+	private char recupererMarqueDeLaPosition(int x, int y) {
+		char marque;
+		if (aUnVaisseauQuiOccupeLaPosition(x, y)) {
+			marque = MARQUE_VAISSEAU;
+		} else {
+			marque = MARQUE_VIDE;
+		}
+		return marque;
+	}
+
+	public String recupererEspaceJeuDansChaineASCII() {
 		StringBuilder espaceDeJeu = new StringBuilder();
 		for (int y = 0; y < hauteur; y++) {
 			for (int x = 0; x < longueur; x++) {
-				if (aUnVaisseauQuiOccupeLaPosition(x, y)) {
-					espaceDeJeu.append('V');
-				} else {
-					espaceDeJeu.append('.');
-				}
+				espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
 			}
-			espaceDeJeu.append('\n');
+			espaceDeJeu.append(MARQUE_FIN_LIGNE);
 		}
 		return espaceDeJeu.toString();
+	}
+
+	@Override
+	public String toString() {
+		return recupererEspaceJeuDansChaineASCII();
 	}
 
 }
